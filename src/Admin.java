1e262b328f1bf5af5e7b4196ad31aac343d5053e
Islabre <islabre1426@gmail.com>
2025-05-13 14:46:18 +0700

    Initial implementation of User-related classes

---
+++ src/Admin.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Admin extends User {
+    public Admin(String userId, String userName, String userPassword, String userRegisterTime, String userRole) {
+        super(userId, userName, userPassword, userRegisterTime, userRole);
+    }
+
+    public Admin() {
+
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
---
+++ src/Customer.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Customer extends User {
+    private String userEmail;
+    private String userMobile;
+
+    public Customer(String userId, String userName, String userPassword,
+                    String userRegisterTime, String userRole,
+                    String userEmail, String userMobile) {
+        super(userId, userName, userPassword, userRegisterTime, userRole);
+        this.userEmail = userEmail;
+        this.userMobile = userMobile;
+    }
+
+    public Customer() {
+
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
