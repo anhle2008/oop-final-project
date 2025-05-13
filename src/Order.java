bce505d67096bfd8d6f340d33a06ff7196d91ba6
Islabre <islabre1426@gmail.com>
2025-05-13 15:13:38 +0700

    Adding initial Order and Product implementation

---
+++ src/Order.java
@@ -0,0 +1,25 @@
+package src;
+
+public class Order {
+    private String orderId;
+    private String userId;
+    private String proId;
+    private String orderTime;
+
+    public Order(String orderId, String userId, String proId, String orderTime) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.proId = proId;
+        this.orderTime = orderTime;
+    }
+
+    public Order() {
+
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,35 @@
+package src;
+
+public class Product {
+    private String proId;
+    private String proModel;
+    private String proCategory;
+    private String proName;
+    private double proCurrentPrice;
+    private double proRawPrice;
+    private double proDiscount;
+    private int proLikesCount;
+
+    public Product(String proId, String proModel, String proCategory,
+                String proName, double proCurrentPrice, double proRawPrice,
+                double proDiscount, int proLikesCount) {
+        this.proId = proId;
+        this.proModel = proModel;
+        this.proCategory = proCategory;
+        this.proName = proName;
+        this.proCurrentPrice = proCurrentPrice;
+        this.proRawPrice = proRawPrice;
+        this.proDiscount = proDiscount;
+        this.proLikesCount = proLikesCount;
+    }
+
+    public Product() {
+
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
