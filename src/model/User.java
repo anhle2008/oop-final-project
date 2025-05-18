0a251371e91d8c04182b6f0c730630b97534e279
LAPTOP <lenhatanh208@gmail.com>
2025-05-18 22:12:10 +0700

    update admin function

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,6 @@
-package src.model;
+package model;
+
+import java.util.Objects;
 
 public abstract class User {
     protected String userId;
@@ -7,7 +9,6 @@ public abstract class User {
     protected String userRegisterTime;
     protected String userRole;
 
-    // Constructor
     public User(String userId, String userName, String userPassword,
                 String userRegisterTime, String userRole) {
         this.userId = userId;
@@ -17,33 +18,43 @@ public abstract class User {
         this.userRole = userRole;
     }
 
-    // Default constructor
     public User() {
-        this.userId = "u_0000000000";
-        this.userName = "default_user";
-        this.userPassword = "password";
-        this.userRegisterTime = "01-01-2025_00:00:00";
-        this.userRole = "customer";
+        this("", "", "", "", "customer");
     }
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
+    // Getters and Setters
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
+    public String getUserPassword() { return userPassword; }
+    public void setUserPassword(String userPassword) { this.userPassword = userPassword; }
+    public String getUserRegisterTime() { return userRegisterTime; }
+    public void setUserRegisterTime(String userRegisterTime) { this.userRegisterTime = userRegisterTime; }
+    public String getUserRole() { return userRole; }
+    public void setUserRole(String userRole) { this.userRole = userRole; }
 
     @Override
     public String toString() {
-        return String.format(
-                "{\"user_id\":\"%s\", \"user_name\":\"%s\", \"user_password\":\"%s\", \"user_register_time\":\"%s\", \"user_role\":\"%s\"}",
-                userId, userName, userPassword, userRegisterTime, userRole
-        );
+        return String.format("{\"user_id\":\"%s\", \"user_name\":\"%s\", \"user_password\":\"%s\", " +
+                        "\"user_register_time\":\"%s\", \"user_role\":\"%s\"}",
+                userId, userName, userPassword, userRegisterTime, userRole);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId) &&
+                Objects.equals(userName, user.userName) &&
+                Objects.equals(userPassword, user.userPassword) &&
+                Objects.equals(userRegisterTime, user.userRegisterTime) &&
+                Objects.equals(userRole, user.userRole);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, userName, userPassword, userRegisterTime, userRole);
     }
 }
